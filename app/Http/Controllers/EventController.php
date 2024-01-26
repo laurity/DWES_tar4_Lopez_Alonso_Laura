6d3b3282f6db38c1d90a68c7d8b9997f77363023
Laura López <99351746+laurity@users.noreply.github.com>
2024-01-26 09:46:57 +0100

    funciona!

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Event;
+use App\Models\Participant;
 
 class EventController extends Controller
 {
--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,11 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\EventController;
+use App\Http\Controllers\OrganizerController;
+use App\Http\Controllers\ParticipantController;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\UserDetailController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +19,14 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::apiResource('event', EventController::class);
+Route::apiResource('organizer', OrganizerController::class);
+Route::apiResource('participan', ParticipantController::class);
+Route::apiResource('user', UserController::class);
+Route::apiResource('userdetail', UserDetailController::class);
+
+
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
